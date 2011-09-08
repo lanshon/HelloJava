4e8bb88502af739a32baa7a816572fcb2529a5cd
lanshon <lanshon@gmail.com>
2011-09-08 13:15:10 +0800

    edit HelloJava

--- HelloJava/src/org/lan/practise/HelloJava.java
+++ HelloJava/src/org/lan/practise/HelloJava.java
@@ -6,8 +6,9 @@ public class HelloJava {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("HelloJava...Git");
+		
 	}
 
 }
